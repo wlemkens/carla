be630d54a3a10179ea0febcf9e00c1a0741b503c
Wim Lemkens <wim.lemkens@trace.vision>
2025-06-29 09:13:02 +0200

    Fixed boost download

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -171,7 +171,7 @@ carla_dependency_option (BOOST_GIL_BUILD_HEADER_TESTS OFF)
 carla_dependency_add(
   boost
   ${CARLA_BOOST_TAG}
-  https://github.com/boostorg/boost/releases/download/${CARLA_BOOST_TAG}/${CARLA_BOOST_TAG}.zip
+  https://github.com/boostorg/boost/releases/download/${CARLA_BOOST_TAG}/${CARLA_BOOST_TAG}.tar.gz
   https://github.com/boostorg/boost.git
 )
 
